8ec1efa7d6d0cc047b683cea735e45a38a405546
VladKondratenko <test@example.com>
2025-09-03 21:18:57 +0300

    initial commit: add basic structure for KutokAccounting app with main page, routing, and styles

---
+++ KutokAccounting/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿namespace KutokAccounting;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+	}
+
+	protected override Window CreateWindow(IActivationState? activationState)
+	{
+		return new Window(new MainPage())
+		{
+			Title = "KutokAccounting"
+		};
+	}
+}
---
+++ KutokAccounting/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace KutokAccounting;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
